60f5fb354fcb990b85787d175b6a9babe42746d4
Snehal Chauhan <48386561+snehalchauhan17@users.noreply.github.com>
2024-01-12 22:49:34 +0530

    Design Changes

--- README.md
+++ README.md
@@ -36,3 +36,16 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
           "options": {
             "browserTarget": "NodeAngularUI:build"
           } -->
+ "serve": {
+          "builder": "@angular-devkit/build-angular:dev-server",
+          "configurations": {
+            "production": {
+              "buildTarget": "RMSNodeJS:build:production"
+            },
+            "development": {
+              "buildTarget": "RMSNodeJS:build:development"
+            }
+          },
+          "defaultConfiguration": "development"
+          
+        },
--- src/app/dashboard/data-entry/data-entry.component.ts
+++ src/app/dashboard/data-entry/data-entry.component.ts
@@ -8,24 +8,38 @@ import { AppServiceService } from 'src/app/app-service.service';
 })
 export class DataEntryComponent {
   posts: any[] | undefined;
-
+id:number =1;
   constructor(private apiService: AppServiceService) {}
 
   ngOnInit() {
-//this.getData();
-    this.apiService.getPosts().subscribe(
-      (data: any[]) => {
-        console.log(data);
-        this.posts = data;
-      },
-      (error) => {
-        // Handle error
-        console.error('Error in component:', error);
-      }
-    );
+this.getData();
+   this.fetchDataById(this.id);
   }
 getData(){
-
+  this.apiService.getPosts().subscribe(
+    (data: any[]) => {
+      console.log(data);
+      this.posts = data;
+    },
+    (error) => {
+      // Handle error
+      console.error('Error in component:', error);
+    }
+  );
+}
+fetchDataById(id: number): void {
+  this.apiService.getPostById(id).subscribe(
+    (data) => {
+      // Handle the response data
+      console.log(data);
+this.id =data;  
+      // You can perform any additional logic or update component properties here
+    },
+    (error) => {
+      // Handle errors
+      console.error(error);
+    }
+  );
 }
   title = environment.title;
   apiURL = environment.apiURL;
