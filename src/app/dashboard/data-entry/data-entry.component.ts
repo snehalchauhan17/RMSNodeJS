0b03194fdb0c498971a9b5a081b9d34bffa0bcbc
Dipakpatel787 <dipakpatel8055@gmail.com>
2024-01-12 11:43:48 +0530

    added url in dataentry

--- src/app/dashboard/data-entry/data-entry.component.ts
+++ src/app/dashboard/data-entry/data-entry.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-data-entry',
   templateUrl: './data-entry.component.html',
-  styleUrls: ['./data-entry.component.css']
+  styleUrls: ['./data-entry.component.css'],
 })
 export class DataEntryComponent {
-
+  title = environment.title;
+  apiURL = environment.apiURL;
 }
